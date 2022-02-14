b19458f873f57c4f1ec2338a81629e81fc8ffae6
WooiLing <wooi_ling_chin@dell.com>
2022-02-14 20:17:37 +0800

    updated homepage, login page

---
+++ homePage/urls.py
@@ -0,0 +1,10 @@
+from django.urls import include, re_path 
+from homePage import views
+
+app_name = 'homePage'
+
+urlpatterns=[
+    re_path(r'^register/$',views.register,name='register'), #url name
+    re_path(r'^user_login/$',views.user_login,name='user_login'),
+    re_path(r'^dashboard/$',views.dashboard,name='dashboard'),
+]
